c0a8eacceddd672b75674becb5741f5adfcfa794
Raruto <Theo.95@hotmail.it>
2020-02-17 21:32:54 +0100

    wpcs

--- index.php
+++ index.php
@@ -6,89 +6,103 @@
  * Version: 0.85
  * Author: Ian James
  * Author URI: http://inj.ms
+ * GitHub Plugin URI: https://github.com/injms/iubenda-for-wp
  */
 
-
-// Variables and other settings
-// =============================================================================
-
-// Time when a file needs to be refetched from the JSON source
-// -----------------------------------------------------------
-$timelapse = 86400; // 24 hours in seconds ( 60 x 60 x 24 )
-
-	
-// Useful functions
-// =============================================================================
-
-
-// check the age of a file, and return true if less than $timelapse old
-// --------------------------------------------------------------------
-function injms_check_age($file_age, $timelapse) {
-	$current_time = time();
-	$time_difference = $current_time - $file_age;
-
-	if ( $time_difference < $timelapse ) {
-		return true;
-	}
-	else {
-		return false;
-	}
-};
-
-
-// GET the $url and returns it
-// ---------------------------
-function injms_curl($url) {
-	$ch = curl_init();
-	$timeout = 5;
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-	$data = curl_exec($ch);
-	curl_close($ch);
-	return $data;
+/**
+ * Exit if called directly.
+ */
+if ( ! defined( 'WPINC' ) ) {
+	die;
 }
 
 
-// Save the supplied JSON
-// ----------------------
-function injms_save_json( $json, $filename ) {
-	file_put_contents($filename, $json);
+/**
+ * Time when a file needs to be refetched from the JSON source
+ *
+ * @var integer $timelapse
+ */
+$timelapse = 86400; // 24 hours in seconds ( 60 x 60 x 24 )
+
+
+/**
+ * Check the age of a file, and return true if less than $timelapse old
+ *
+ * @param  integer $file_age  file age.
+ * @param  integer $timelapse timelapse.
+ * @return bool true if less than $timelapse old.
+ */
+function injms_check_age( $file_age, $timelapse ) {
+	$current_time    = time();
+	$time_difference = $current_time - $file_age;
+
+	return $time_difference < $timelapse;
 };
 
+/**
+ * GET the $url and returns it
+ *
+ * @param  string $url remote url.
+ * @return object $data curl response.
+ */
+function injms_curl( $url ) {
+	$ch      = curl_init();
+	$timeout = 5;
+	curl_setopt( $ch, CURLOPT_URL, $url );
+	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+	curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $timeout );
+	$data = curl_exec( $ch );
+	curl_close( $ch );
+	return $data;
+}
 
-// The main function - call this to embed the policy anywhere in your theme
-// eg echo injms_iubenda( 123456, true, 'black' );
-//  - first variable is the policy ID
-//	- the second is true / false for whether a table of contents is generated 
-//	with some jQuery
-//	- the third is what colour the fallback JavaScript button should be - black, 
-// white, or nostyle. Note the nostyle needs a pro account.
-// =============================================================================
-function injms_iubenda( $policy_id, $toc, $theme ){
+/**
+ * Save the supplied JSON
+ *
+ * @param  object $json     file data
+ * @param  string $filename file name
+ * @return void
+ */
+function injms_save_json( $json, $filename ) {
+	file_put_contents( $filename, $json );
+};
+
+/**
+ * The main function - call this to embed the policy anywhere in your theme
+ * eg echo injms_iubenda( 123456, true, 'black' );
+ *
+ * @param  integer $policy_id first variable is the policy ID.
+ * @param  bool    $toc       true / false for whether a table of contents
+ *                            is generated with some jQuery.
+ * @param  string  $theme     what colour the fallback JavaScript button
+ *                            should be: black|white|nostyle.
+ *                            Note the nostyle needs a pro account.
+ * @return string iubenda policy.
+ */
+function injms_iubenda( $policy_id, $toc, $theme ) {
 
 	$injms_upload_dir = wp_upload_dir();
 
-	$injms_iubenda = new stdClass;
+	$injms_iubenda            = new stdClass();
 	$injms_iubenda->directory = $injms_upload_dir['basedir'] . '/injms-iubenda/';
-	$injms_iubenda->file = $injms_iubenda->directory . $policy_id . '.json';
+	$injms_iubenda->file      = $injms_iubenda->directory . $policy_id . '.json';
 
-	// Check to make sure the directory that we're going to cache the JSON
-	// file in exists
-	if ( !file_exists( $injms_iubenda->directory ) ) {
+	// Check to make sure the directory that
+	// we're going to cache the JSON file in exists.
+	if ( ! file_exists( $injms_iubenda->directory ) ) {
 		mkdir( $injms_iubenda->directory, '0755', true );
 	}
 
-	// Check to make sure a file isn't already cached
-	if ( !file_exists( $injms_iubenda->file ) ) {
+	// Check to make sure a file isn't already cached.
+	if ( ! file_exists( $injms_iubenda->file ) ) {
 
 		$injms_iubenda->json = injms_curl( 'https://www.iubenda.com/api/privacy-policy/' . $policy_id . '/no-markup' );
 
 		$injms_iubenda->output_php = json_decode( $injms_iubenda->json );
 
-		// We only want to save this if it's been successful. This is the first part of the
-		// return JSON : { success: 1, ...}
-		// If not, we serve the JavaScript fallback and break
+		// We only want to save this if it's been successful.
+		// This is the first part of the return JSON : { success: 1, ...}
+		// If not, we serve the JavaScript fallback and break.
 		if ( $injms_iubenda->output_php->success == true ) {
 			injms_save_json( $injms_iubenda->json, $injms_iubenda->file );
 		} else {
@@ -97,10 +111,10 @@ function injms_iubenda( $policy_id, $toc, $theme ){
 	}
 
 	$injms_iubenda->output_json = file_get_contents( $injms_iubenda->file );
-	$injms_iubenda->output_php = json_decode( $injms_iubenda->output_json );
+	$injms_iubenda->output_php  = json_decode( $injms_iubenda->output_json );
 
-	if ( $injms_iubenda->output_php->success == true AND $toc == true) {
-		return preg_replace('/<br \\/>/', '</p><p>', $injms_iubenda->output_php->content) . 
+	if ( $injms_iubenda->output_php->success == true and $toc == true ) {
+		return preg_replace( '/<br \\/>/', '</p><p>', $injms_iubenda->output_php->content ) .
 			"<script>
 				(function($){
 					var policyHeadings = $('#policy-contents :header:not(h1)'),
@@ -119,8 +133,6 @@ function injms_iubenda( $policy_id, $toc, $theme ){
 				})(jQuery);
 			</script>";
 	} elseif ( $injms_iubenda->output_php->success == true ) {
-		return preg_replace('/<br \\/>/', '</p><p>', $injms_iubenda->output_php->content);
+		return preg_replace( '/<br \\/>/', '</p><p>', $injms_iubenda->output_php->content );
 	}
 }
-
-?>
